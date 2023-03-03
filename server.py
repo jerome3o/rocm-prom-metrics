7ca280477bceef5e00be1750118c79f6fb65dc89
Jerome Swannack <jeromeswannack@gmail.com>
2023-03-04 09:18:56 +1300

    Added gauge creation

--- server.py
+++ server.py
@@ -1,5 +1,7 @@
 import subprocess
 import json
+import re
+from typing import Dict
 from prometheus_client import start_http_server, Gauge
 
 _flags = [
@@ -16,7 +18,7 @@ _flags = [
 ]
 
 
-def get_smi_output() -> dict:
+def get_smi_output() -> Dict[str, Dict[str, str]]:
     """
     Example output:
     {
@@ -47,12 +49,48 @@ def get_smi_output() -> dict:
     return json.loads(output_str)
 
 
+def _get_prom_friendly_metric_name(metric_name: str) -> str:
+    """
+    Convert metric names to prometheus friendly names
+    """
+    metric_name = re.sub(r"[^a-zA-Z0-9_]", "_", metric_name).lower()
+
+    # remove trailing/leading underscores
+    metric_name = metric_name.strip("_")
+
+    # remove double underscores
+    metric_name = re.sub(r"__+", "_", metric_name)
+
+    return metric_name
+
+
+def _define_gauges(output: dict) -> Dict[str, Gauge]:
+    # card will be a label, so get unqiue metrics across all cards
+    metric_info = list(set([
+        (get_prom_friendly_metric_name(metric_name), metric_name)
+        for card in output.values()
+        for metric_name in card.keys()
+    ]))
+
+    # define gauges from metric_info
+    return {
+        metric_name: Gauge(
+            metric_name,
+            raw_name,
+            labelnames=["gpu"],
+        )
+        for metric_name, raw_name in metric_info
+    }
+
+
 def main():
    
     # get output dict
     output = get_smi_output()
 
 
+    # start prometheus server
+    start_http_server(8000)
 
 if __name__ == "__main__":
     import logging
